5d84b271b97b6589fc06252247e1542daa90292a
Romay <honuvorrosemary@gmail.com>
2025-05-20 20:05:31 +0000

    Fix JDBC URL port issue by setting default port to 5432

--- src/main/java/com/logistics/LogisticsFuture/config/DatabaseConfig.java
+++ src/main/java/com/logistics/LogisticsFuture/config/DatabaseConfig.java
@@ -15,11 +15,18 @@ public class DatabaseConfig {
 
     @Bean
     public DataSource dataSource() throws Exception {
+        if (databaseUrl == null || databaseUrl.isEmpty()) {
+            throw new IllegalStateException("DATABASE_URL environment variable is not set");
+        }
+
+        // Parse the Render DATABASE_URL
         URI dbUri = new URI(databaseUrl);
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        // Use 5432 as the default port if none is specified
+        String port = dbUri.getPort() == -1 ? "5432" : String.valueOf(dbUri.getPort());
+        String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath() + "?sslmode=require";
 
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(jdbcUrl);
